0e58871888d2a6b7f12d97bc7274d83d4ac7e6d1
Salakhoff <salakhoff.ilfat@gmail.com>
2023-01-19 00:32:51 +0300

    preparation for work

---
+++ CustomTextField/AuthViewController.swift
@@ -0,0 +1,39 @@
+//
+//  ViewController.swift
+//  CustomTextField
+//
+//  Created by Ильфат Салахов on 19.01.2023.
+//
+
+import UIKit
+
+final class AuthViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setupView()
+    }
+}
+
+// MARK: -SettingView
+private extension AuthViewController {
+    func setupView() {
+        addSubViews()
+        setupLayout()
+    }
+}
+
+// MARK: Setting
+
+private extension AuthViewController {
+    func addSubViews() {
+        
+    }
+}
+
+// MARK: Layout
+private extension AuthViewController {
+    func setupLayout() {
+        
+    }
+}
